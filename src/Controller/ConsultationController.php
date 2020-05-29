731e3f7ebe61b9fbd1b33be44df421e61009c55d
Alexandru-Ionut Togorean <alex.togorean@gmail.com>
2020-05-29 12:47:11 +0300

    Added sending emails at consultation creation.

--- src/Controller/ConsultationController.php
+++ src/Controller/ConsultationController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Consultation;
 use App\Form\ConsultationType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConsultationController extends AbstractController
@@ -13,7 +15,7 @@ class ConsultationController extends AbstractController
     /**
      * @Route("/consultanta-on-line", name="app_consultation")
      */
-    public function index(Request $request)
+    public function index(Request $request, MailerInterface $mailer)
     {
         $consultation = new Consultation();
         $form = $this->createForm(ConsultationType::class, $consultation);
@@ -21,8 +23,23 @@ class ConsultationController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($form->getData());
+            $consultation = $form->getData();
+            $entityManager->persist($consultation);
             $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($this->getParameter('mail_sender'))
+                ->to($consultation->getEmail())
+                ->subject('Consultație online cu Dr. Monica Burlacu')
+                ->htmlTemplate('emails/consultation-created.html.twig');
+            $mailer->send($email);
+
+            $email = (new TemplatedEmail())
+                ->from($this->getParameter('mail_sender'))
+                ->to($this->getParameter('mail_doctor'))
+                ->subject('Consultație online înregistrată')
+                ->htmlTemplate('emails/consultation-created-doctor.html.twig');
+            $mailer->send($email);
 
             $consultation = new Consultation();
             $form = $this->createForm(ConsultationType::class, $consultation);
